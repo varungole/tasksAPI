61e4d4e310294eeb56f67cb4895ac3399441237f
Varun Gole <varungole1999@gmail.com>
2025-03-03 16:19:31 -0500

    no need of mainapp

--- src/main/java/com/taskManagerBackend/backend/MainApp.java
+++ src/main/java/com/taskManagerBackend/backend/MainApp.java
@@ -6,14 +6,6 @@ import io.vertx.core.VertxOptions;
 public class MainApp {
 
   public static void main(String[] args) {
-    Vertx vertx = Vertx.vertx(new VertxOptions().setBlockedThreadCheckInterval(1000));
 
-    vertx.deployVerticle(MainVerticle.class.getName(), stringAsyncResult -> {
-      if(stringAsyncResult.succeeded()) {
-        System.out.println("Succesfully deployed all verticles");
-      } else {
-        System.out.println("Failed to deploy verticles " + stringAsyncResult.cause());
-      }
-    });
   }
 }
